d190b75505df01a0d96da7da5d7dd61bcb83d03d
Evion Cane <evion.cane@lhind.dlh.de>
2021-03-31 17:20:53 +0200

    Creation of MinAvgTwoSlice

---
+++ src/lesson5/MinAvgTwoSlice.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class MinAvgTwoSlice {
+    public static void main(String[] args) {
+        MinAvgTwoSlice minAvgTwoSlice = new MinAvgTwoSlice();
+
+    }
+
+    public int solution(int[] A) {
+        return 0;
+    }
+}
